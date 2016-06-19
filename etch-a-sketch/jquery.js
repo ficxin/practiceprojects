5165c5318f47987bde370ae2df76de02397a86a7
ficxin <xinjohnzhang@gmail.com>
2016-06-19 14:22:24 -0700

    Add gray-in panel effect

--- etch-a-sketch/jquery.js
+++ etch-a-sketch/jquery.js
@@ -1,19 +1,24 @@
 $(document).ready(function(){
+	$('html').css({
+		"height": "100%"
+	});
+
 	$('body').css({
-		"display": "flex"
+		"display": "flex",
+		"height": "100%"
 	});
 
 	$('.panel-wrapper').css({
 		"background-color": "#B03A2E",
-		"width": "700px",
-		"height": "700px",
-		"margin": "5% auto",
+		"width": "600px",
+		"height": "600px",
+		"margin": "auto",
 		"display": "flex",
 		"border-radius": "16px"
 	});
 
 	$('.panel-container').css({
-		"width": "600px",
+		"width": "500px",
 		"margin": "auto",
 		"padding": "4px",
 		"border-radius": "4px",
@@ -25,10 +30,9 @@ $(document).ready(function(){
 	drawPanels(containerWidth);
 });
 
+
 function drawPanels(containerWidth, size=28) {
-	console.log("hi");
 	var panelSideLength = containerWidth/size - 2;
-	console.log(size);
 	for(var i = 0; i < size; i++) {
 		for(var j = 0; j < size; j++)
 			$('.panel-container').append('<div class="panel"></div>');
@@ -43,11 +47,17 @@ function drawPanels(containerWidth, size=28) {
 		"background-color": "#17202A",
 		"opacity": "0.1"
 	})
-		.hover(function() {
+		.mouseenter(function() {
 			var $currentOpacity = $(this).css('opacity');
-			if ($currentOpacity < 1)
-				$('.panel').css('opacity', $currentOpacity + 0.1);
+			if ($currentOpacity < 1) {
+				var newOpacity = (Number($currentOpacity) + 0.1).toString();
+				$(this).css('opacity', newOpacity);
+			}
 		});
-
 }
+
+
+
+
+
 	
