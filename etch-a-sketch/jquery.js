bd3bd312652873ea8a145290bbb805ef702c5e3a
ficxin <xinjohnzhang@gmail.com>
2016-06-19 18:15:08 -0700

    Add buttons to set color option

--- etch-a-sketch/jquery.js
+++ etch-a-sketch/jquery.js
@@ -1,18 +1,56 @@
 $(document).ready(function(){
+	var htmlPanel = '<div class="panel-wrapper">' +
+									'<section class="panel-container"></section>' +
+									'</div>'
+
+	var htmlButtons = '<div class="panel-settings">' +
+									'<button class="gray"></button>' +
+									'<button class="red"></button>' +
+									'<button class="yellow"></button>' +
+									'<button class="green"></button>' +
+									'<button class="blue"></button>' +
+									'</div>'
+									
+  $('body').append(htmlButtons);
+								
+	$('body').append(htmlPanel);
+
 	$('html').css({
 		"height": "100%"
 	});
 
 	$('body').css({
+		"margin": "0",
 		"display": "flex",
+		"justify-content": "center",
+		"align-items": "center",
 		"height": "100%"
 	});
 
+	$('.panel-settings').css({
+		"min-width": "60px",
+		"height": "200px",
+		"display": "flex",
+		"flex-direction": "column",
+		"justify-content": "space-between",
+		"align-items": "flex-start"
+	});
+  
+  $('button').css({
+  	"height": "32px",
+  	"width": "32px",
+  	"border-radius": "50%"
+  });
+
+  $('.yellow').css('background-color', '#F7DC6F');
+  $('.blue').css('background-color', '#3498DB');
+  $('.green').css('background-color', '#7DCEA0');
+  $('.red').css('background-color', '#E74C3C');
+
 	$('.panel-wrapper').css({
 		"background-color": "#B03A2E",
-		"width": "600px",
+		"min-width": "600px",
 		"height": "600px",
-		"margin": "auto",
 		"display": "flex",
 		"border-radius": "16px"
 	});
@@ -28,10 +66,35 @@ $(document).ready(function(){
 	var containerWidth = $('.panel-container').width();
 
 	drawPanels(containerWidth);
+
+	$('.gray').on('click', function() {
+		clearPanels();
+		drawPanels(containerWidth);
+	});
+
+	$('.red').on('click', function() {
+		clearPanels();
+		drawPanels(containerWidth, "#E74C3C");
+	});
+
+	$('.yellow').on('click', function() {
+		clearPanels();
+		drawPanels(containerWidth, "#F4D03F");
+	});
+
+	$('.green').on('click', function() {
+		clearPanels();
+		drawPanels(containerWidth, "#229954");
+	});
+
+	$('.blue').on('click', function() {
+		clearPanels();
+		drawPanels(containerWidth, "#3498DB");
+	});
 });
 
 
-function drawPanels(containerWidth, size=28) {
+function drawPanels(containerWidth, color="#17202A", size=28) {
 	var panelSideLength = containerWidth/size - 2;
 	for(var i = 0; i < size; i++) {
 		for(var j = 0; j < size; j++)
@@ -48,6 +111,7 @@ function drawPanels(containerWidth, size=28) {
 		"opacity": "0.1"
 	})
 		.mouseenter(function() {
+			$(this).css('background-color', color);
 			var $currentOpacity = $(this).css('opacity');
 			if ($currentOpacity < 1) {
 				var newOpacity = (Number($currentOpacity) + 0.1).toString();
@@ -56,8 +120,6 @@ function drawPanels(containerWidth, size=28) {
 		});
 }
 
-
-
-
-
-	
+function clearPanels() {
+	$('.panel').remove();
+};
