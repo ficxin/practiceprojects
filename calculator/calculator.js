01d17f0c7d2bd4434d4a5be7004b24ba24441460
ficxin <xinjohnzhang@gmail.com>
2016-06-22 01:45:57 -0700

    Display numbers when number pad is pressed and add the four basic math functions

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,3 +1,83 @@
+var defaultValue = '<span id="decimal">0.</span>',
+ 		currentValue = "",
+		totalValue = "";
+
+document.body.addEventListener("click", function(event) {
+	if (event.target.nodeName == 'BUTTON')
+		if (Number(event.target.textContent)) {
+			currentValue += event.target.textContent;
+			display(currentValue, digitalize);
+		} 
+		else if (event.target.textContent == "CA") {
+			currentValue = "";
+			totalValue = "";
+			display(defaultValue);
+		}
+		else if (event.target.textContent == "+") {
+			if (currentValue) {
+				if (totalValue) {
+					totalValue = add(Number(totalValue), Number(currentValue));
+				} else {
+					totalValue = currentValue;
+				}
+				currentValue = "";
+				display(totalValue.toString(), digitalize);
+			}
+		}
+		else if (event.target.textContent == "–") {
+			if (currentValue) {
+				if (totalValue) {
+					totalValue = subtract(Number(totalValue), Number(currentValue));
+				} else {
+					totalValue = currentValue;
+				}
+				currentValue = "";
+				display(totalValue.toString(), digitalize);
+			}
+		}
+		else if (event.target.textContent == "x") {
+			if (currentValue) {
+				if (totalValue) {
+					totalValue = multiply(Number(totalValue), Number(currentValue));
+				} else {
+					totalValue = currentValue;
+				}
+				currentValue = "";
+				display(totalValue.toString(), digitalize);
+			}
+		}
+		else if (event.target.textContent == "÷") {
+			if (currentValue) {
+				if (totalValue) {
+					totalValue = divide(Number(totalValue), Number(currentValue));
+				} else {
+					totalValue = currentValue;
+				}
+				currentValue = "";
+				display(totalValue.toString(), digitalize);
+			}
+		}
+		else if (event.target.textContent == "=") {
+			console.log('equal');
+		}
+});
+
+function digitalize(value) {
+	if (value.length > 9)
+		value = value.slice(0, 9);
+	var span = '<span id="decimal">' + value.slice(value.length-1) + '.</span>';
+	var result = value.slice(0, value.length-1) + span;
+	return result;
+}
+
+function display(value, callback) {
+	if (callback) {
+		document.getElementById('led').innerHTML = callback(value);
+	} else {
+		document.getElementById('led').innerHTML = value;
+	}
+}
+
 function add(n1, n2) {
 	return n1 + n2;
 };
@@ -13,11 +93,3 @@ function multiply(n1, n2) {
 function divide(n1, n2) {
 	return n1 / n2;
 };
-
-$(document).ready(function() {
-
-	$('.button').on('click', function() {
-		console.log($(this).text());
-	})
-
-});
